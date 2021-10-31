f14c7677baebf1f773b8c7fc395283fa3a13f245
DefyCab <love.bellinder@gmail.com>
2021-10-31 20:05:17 +0100

    Error seeking

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,19 +1,16 @@
 describe('User can check their Bmi', () => {
-    it('user can enter weight and height', () => {
+    
+            it('user can enter weight and height', () => {
         cy.visit('http://localhost:3474')
         cy.contains('BMI Calculator')
-        cy.get('#weight').type('83')
-        cy.get('#height').type('177')
+        cy.get('#weight').type('90')
+        cy.get('#height').type('186')
         cy.get('#Calculate').click()
-    });
-
-        it('displays a BMI value of 25', () => {
-            cy.get('#results').should('contain', '25')
+    })
         
-        });   
-    
-        
-    
 
+    it('displays a BMI value of 26.01', () => {
+        cy.get('#results').should('contain', '26.01')
+    })
 
-});
+})
